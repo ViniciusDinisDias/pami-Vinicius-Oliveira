15197058d1cba518f61735f0f6137db44badc2e6
Vinicius Oliveira <vinixoddias@gmail.com>
2025-08-20 20:58:59 -0300

    Finaliza app

--- StickerSmash/app/_layout.tsx
+++ StickerSmash/app/_layout.tsx
@@ -1,20 +1,26 @@
 import { Stack } from 'expo-router';
 import { LogBox  } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { GestureHandlerRootView  } from 'react-native-gesture-handler';
 
 LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen 
-      name="(tabs)" 
-      options={{ 
-        headerShown: false,
-        }} 
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar style="light"/>
+      <Stack>
+        <Stack.Screen 
+          name="(tabs)" 
+          options={{ 
+           headerShown: false,
+          }} 
         />
         <Stack.Screen 
           name="+not-found" 
           options={{}} />
-    </Stack>
+      </Stack>
+    </GestureHandlerRootView>
+  
   );
 }
